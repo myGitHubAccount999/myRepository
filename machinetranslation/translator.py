395952b14840241dff162a8667f45037cd4f3dc7
myGitHubAccount999 <99669544+myGitHubAccount999@users.noreply.github.com>
2022-02-21 11:23:18 +0100

    Add files via upload

---
+++ machinetranslation/translator.py
@@ -0,0 +1,28 @@
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+
+lt = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
+
+lt.set_service_url(url)
+
+
+def english_to_french(english_text):
+    transl = lt.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = transl['translations'][0]['translation']
+    return french_text
+
+
+def french_to_english(french_text):
+    transl = lt.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = transl['translations'][0]['translation']
+    return english_text
+
